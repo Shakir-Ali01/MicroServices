7e719ec2dec0b33290ee7cbffd72e920eeba1575
shakir-Ali01 <connectwithshakirali@gmail.com>
2024-12-10 09:50:45 +0530

    profile concept adding

---
+++ section6/v2-spring-cloud-config/accounts/src/main/java/com/ms/accounts/constants/AccountConstants.java
@@ -0,0 +1,30 @@
+package com.ms.accounts.constants;
+
+public class AccountConstants {
+    private AccountConstants(){
+        //restrict instantiation
+    }
+    public static final String SAVINGS = "savings";
+    public static final String ADDRESS = "123 street new delhi";
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Account created successfully";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request processed successfully";
+    public static final String STATUS_404 = "404";
+    public static final String MESSAGE_404 = "Account not found";
+
+    public static final String  STATUS_417 = "417";
+    public static final String  MESSAGE_417_UPDATE= "Update operation failed. Please try again or contact Dev team";
+    public static final String  MESSAGE_417_DELETE= "Delete operation failed. Please try again or contact Dev team";
+
+
+
+
+
+
+
+    public static final String STATUS_500 = "500";
+    public static final String MESSAGE_500 = "An error occured . Please Try again or contact support team.";
+
+
+}
