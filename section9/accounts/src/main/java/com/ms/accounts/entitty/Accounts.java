5bb17beb9b6c37bb8e2344f4cf21434aa1888ad8
shakir-Ali01 <connectwithshakirali@gmail.com>
2025-02-05 11:16:30 +0530

    Applying GatewayServer custom filter

---
+++ section9/accounts/src/main/java/com/ms/accounts/entitty/Accounts.java
@@ -0,0 +1,17 @@
+package com.ms.accounts.entitty;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter@Setter@ToString@AllArgsConstructor@NoArgsConstructor
+public class Accounts extends BaseEntity {
+
+    @Column(name = "customer_id")
+    private Long customerId;
+    @Id
+    private Long accountNumber;
+    private String accountType;
+    private String branchAddress;
+
+}
---
+++ section9/configServer/src/test/java/com/msbyte/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msbyte.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
